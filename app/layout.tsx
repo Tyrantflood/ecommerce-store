785cc21e37cef034222dab98ebeba019cb903f11
Tyrantflood <princeobi124@gmail.com>
2023-08-04 04:15:44 +0100

    nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        {children}
         <Navbar />
+        {children}
         <Footer />
       </body>
     </html>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+import Container from "./ui/container";
+import MainNav from "./main-nav";
+
 const Navbar = () => {
-    return (  
-        <div className=" border-b"></div>
-    );
-}
- 
+  return (
+    <div className=" border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href={"/"} className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className=" font-bold text-xl ">STORE</p>
+          </Link>
+          <MainNav data={[]} />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
 export default Navbar;
