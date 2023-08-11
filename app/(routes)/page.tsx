2fe70cad145d5e6045e7c0f9ab62c14894684a9c
Tyrantflood <princeobi124@gmail.com>
2023-08-11 05:32:58 +0100

    billboard area

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,18 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("7093658d-12e1-44d3-ad41-dbc4681fadf9");
+
   return (
-    <div>
-      <div>Hello Store</div>
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   );
 };
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,8 @@ import MainNav from "./main-nav";
 import getCategories from "@/actions/get-categories";
 import NavbarActions from "./navbar-actions";
 
+export const revalidate = 0;
+
 const Navbar = async () => {
   const categories = await getCategories();
 
